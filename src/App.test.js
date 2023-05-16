04f9e99d608fac1f9281903d2f75ba1836b39219
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-16 17:02:31 -0500

    test for SiteInfo heading

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,13 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App component", () => {
-    it("renders correct heading", () => {
-      render(<App />);
-      expect(screen.getByRole("heading").textContent).toMatch(/mock store/i);
+    it("renders correct site title", () => {
+        render(<App />);
+        expect(screen.getByRole("heading", {level: 1}).textContent).toMatch(/mock store/i);
     });
-  });
+
+    it("renders correct site info heading", () => {
+        render(<App />);
+        expect(screen.getByRole("heading", {level: 2}).textContent).toMatch(/welcome to my mock store!/i);
+    });
+});
