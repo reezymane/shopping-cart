e60539a6bad928ada29aa3199b796b6905b92d67
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-16 17:02:10 -0500

    add SiteInfo div, Logo div, and footer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Title">
-          <h1>Mock Store</h1>
+        <div className="TitleAndCart">
+          <div className="Title">
+            <h1>Mock Store</h1>
+          </div>
+
+          {/*<Cart />*/}
         </div>
+
+        {/*<Nav />*/}
       </header>
+
+      <div className="MainContainer">
+        <div className="SiteInfo">
+          <h2>Welcome to my Mock Store!</h2>
+          <p></p>
+        </div>
+
+        <div className="Logo">
+          <img alt="Website Logo"/>
+        </div>
+      </div>
+
+      <footer> Copyright <span>&copy;</span> ReezyTech</footer>
     </div>
   );
 }
