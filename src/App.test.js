d521f3d0aa2c53f35c109ffd28c3b37b90e2293f
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-16 16:20:18 -0500

    test App heading render

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
+describe("App component", () => {
+    it("renders correct heading", () => {
+      render(<App />);
+      expect(screen.getByRole("heading").textContent).toMatch(/mock store/i);
+    });
+  });
