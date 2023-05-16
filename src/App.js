ab533ed3c9c926d6e42e6c630727f37390a0ff8f
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-16 16:19:29 -0500

    add div .Title and h1 element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="Title">
+          <h1>Mock Store</h1>
+        </div>
       </header>
     </div>
   );
