bff55c6c1612209096cb7007d49a3872a993f43e
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-17 08:12:33 -0500

    create div Cart and add child img and div

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = (prop) => {
+    return (
+        <div className="Cart">
+            <img alt="Shopping Cart" />
 
+            <div className="CartCount">
+                {prop.cartCount};
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
