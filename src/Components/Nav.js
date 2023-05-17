7d747977c605efb322f1cfb122ea0714a35d7320
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-17 17:16:33 -0500

    add list to nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+    return(
+        <div className="NavBar">
+          <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>  
+        </div>
+        
+    );
+};
+
+export default Nav;
