a08fe2b3dadc7620bc8033c8d9bc95f150a337d6
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-17 07:59:41 -0500

    import Cart component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./Components/Cart";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
             <h1>Mock Store</h1>
           </div>
 
-          {/*<Cart />*/}
+          <Cart />
         </div>
 
         {/*<Nav />*/}
