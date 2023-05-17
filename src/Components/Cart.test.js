2a6601a4d74bd06491c7245c2f6ddc9e3a8d9fa5
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-17 08:28:21 -0500

    create test for img rendering in Cart

---
+++ src/Components/Cart.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Cart from "./Cart";
+
+describe ("Cart component", () => {
+    it("renders the correct image with the expected src", () => {
+        render(<Cart />);
+        expect(screen.getByRole("img")).toBeInTheDocument();
+    });
+});
