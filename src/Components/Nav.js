d365f43cd78fb94e1436bdbaa5aba90d482b7036
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-19 08:53:34 -0500

    add brackets to Link import

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return(
         <div className="NavBar">
           <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Shop</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/shop">Shop</Link></li>
                 </ul>
             </nav>  
         </div>
