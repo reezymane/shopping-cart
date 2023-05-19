08255e75b694260596485121b5380a9f2bffc453
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-19 08:19:59 -0500

    import item images

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Watch from "../Assets/watch.png";
+import Necklace from "../Assets/necklace.png";
 
 const Shop = () => {
+    const [goldWatch] = useState({
+        name: "Gold Watch",
+        img: Watch,
+        price: 5200.00,
+        description: "Experience elegance with this gold watch featuring a meticulously crafted watch band, exuding sophistication and comfort. The intricate detailing accentuates its luxurious appeal. Complete with a stylish date dial, this timepiece seamlessly blends classic design with modern functionality. A statement accessory that effortlessly enhances any attire."
+    });
 
+    const [emeraldNecklace] = useState({
+        name: "Emerald Necklace",
+        img: Necklace,
+        price: 7500.00,
+        description: "Adorn yourself with a captivating emerald necklace that exudes timeless beauty. The exquisite emerald gemstone, encased in a delicate pendant, radiates elegance and allure. Paired with a graceful chain, this necklace effortlessly captures attention and adds a touch of sophistication to any ensemble. A stunning piece that embodies luxury and grace."
+    });
 };
 
 export default Shop;
