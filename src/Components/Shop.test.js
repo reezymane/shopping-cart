9584c11e27392337a0a5af411408a14952334579
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-19 08:54:23 -0500

    create Shop component test file

---
+++ src/Components/Shop.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Shop from "./Shop";
+
+describe("Shop component", () => {
+    
+});
