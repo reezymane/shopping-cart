a6c6b10a97cd7131133d1ee54b56dfb8bd169e6b
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-19 08:53:02 -0500

    remove tests for Nav component

--- src/Components/Nav.test.js
+++ src/Components/Nav.test.js
@@ -3,10 +3,5 @@ import '@testing-library/jest-dom/extend-expect';
 import Nav from "./Nav";
 
 describe("Nav component", () => {
-    it("renders correct text for links", () => {
-        render(<Nav />);
-        const links = screen.getAllByRole("listitem");
-        expect(links[0].textContent).toMatch(/home/i);
-        expect(links[1].textContent).toMatch(/shop/i);
-    });
+    
 });
