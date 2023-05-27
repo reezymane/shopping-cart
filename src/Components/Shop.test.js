f2b3a88fd7a5540ca023fc471b52b1ff6d490d83
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-27 08:17:41 -0500

    test that Cards display item information correctly

--- src/Components/Shop.test.js
+++ src/Components/Shop.test.js
@@ -14,4 +14,21 @@ describe("Shop component", () => {
         expect(cards.length).toBe(2);
         // Assert that the component renders the correct number of cards based on the items array
     });
+
+    test('displays item information correctly', () => {
+        render(<Shop />);
+        const cards = screen.getAllByTestId('item-card');
+    
+        // Assert that the card displays the correct item name
+        expect(cards[0]).toHaveTextContent('Gold Watch');
+        expect(cards[1]).toHaveTextContent('Emerald Necklace');
+    
+        // Assert that the card displays the correct item price
+        expect(cards[0]).toHaveTextContent('Price: $5200.00');
+        expect(cards[1]).toHaveTextContent('Price: $7500.00');
+    
+        // Assert that the card displays the correct item description
+        expect(cards[0]).toHaveTextContent('Experience elegance with this gold watch');
+        expect(cards[1]).toHaveTextContent('Adorn yourself with a captivating emerald necklace');
+    });
 });
