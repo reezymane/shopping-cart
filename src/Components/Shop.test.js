4a2cd76669983f157e0fbe370f6bdaf98c235571
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-27 07:52:02 -0500

    Assert that the component renders without error

--- src/Components/Shop.test.js
+++ src/Components/Shop.test.js
@@ -3,5 +3,8 @@ import '@testing-library/jest-dom/extend-expect';
 import Shop from "./Shop";
 
 describe("Shop component", () => {
-    
+    test('renders the component without errors', () => {
+        render(<Shop />);
+        // Assert that the component renders without throwing an error
+    });
 });
