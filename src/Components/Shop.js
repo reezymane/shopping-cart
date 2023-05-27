edf12100a04dca5f328e587d63ec5200ca0b620e
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-27 07:46:57 -0500

    create function to shuffle items and import Card

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Watch from "../Assets/watch.png";
 import Necklace from "../Assets/necklace.png";
+import Card from "./Card";
 
 const Shop = () => {
     const [goldWatch] = useState({
@@ -16,6 +17,36 @@ const Shop = () => {
         price: 7500.00,
         description: "Adorn yourself with a captivating emerald necklace that exudes timeless beauty. The exquisite emerald gemstone, encased in a delicate pendant, radiates elegance and allure. Paired with a graceful chain, this necklace effortlessly captures attention and adds a touch of sophistication to any ensemble. A stunning piece that embodies luxury and grace."
     });
+
+    const items = [goldWatch, emeraldNecklace];
+
+    // Utility function to shuffle the array
+    const shuffle = (array) => {
+        let currentIndex = array.length,
+            temporaryValue,
+            randomIndex;
+  
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+  
+        return array;
+    };
+
+    // Randomize the order of items
+    const shuffledItems = shuffle(items);
+
+    return (
+        <div>
+            {shuffledItems.map((item, index) => (
+                <Card key={index} item={item} />
+            ))}
+        </div>
+    );
 };
 
 export default Shop;
