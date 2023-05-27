4021dc393d9c928abbfacbe8edd82e06e0b5405c
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-27 08:11:02 -0500

    create elements to display info from props

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Card = () => {};
+const Card = (props) => {
+    return (
+        <div className="Card" data-testid="item-card">
+            <h3>{props.item.name}</h3>
+            <img src={props.item.img} alt={props.item.name} />
+            <p>Price: ${props.item.price.toFixed(2)}</p>
+            <p>{props.item.description}</p>
+        </div> 
+    );
+};
 
 export default Card;
