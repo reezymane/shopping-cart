78dd82b0b0c89054dc449d9f35947bc6d0052c77
Deonte McCoy <deontemccoy19@gmail.com>
2023-05-28 08:02:53 -0500

    test Nav display content

--- src/Components/Nav.test.js
+++ src/Components/Nav.test.js
@@ -1,7 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
-import Nav from "./Nav";
+import RouteSwitch from "./RouteSwitch";
 
 describe("Nav component", () => {
-    
+    it('renders Nav component correctly', () => {
+        render(<RouteSwitch />);
+        
+        const homeLink = screen.getByText('Home');
+        const shopLink = screen.getByText('Shop');
+        
+        expect(homeLink).toBeInTheDocument();
+        expect(shopLink).toBeInTheDocument();
+    });
 });
